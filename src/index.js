ef445c92ab05aa04ef572bdd2216bc72aa354edf
Dyary Raoof Bayz <dyary2020@gmail.com>
2021-10-06 00:53:54 +0300

    Add basic index.html and index.js and styles.css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './style.css';
+
+const form = document.forms[0];
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const name = form.elements[0];
+  const score = form.elements[1];
+  const scoresTableBody = document.querySelector('#scores-table-body');
+
+  const tr = document.createElement('tr');
+  const td = document.createElement('td');
+  td.innerHTML = `${name.value} : ${score.value}`;
+  tr.appendChild(td);
+
+  scoresTableBody.appendChild(tr);
+  form.reset();
+});
